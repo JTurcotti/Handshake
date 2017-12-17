b1a7c56f8a1a2e189f0db3ceaa31d14d3e30cec8
Joshua Turcotti <turcotti.joshua@gmail.com>
2017-12-17 12:19:08 -0500

    initial commit

---
+++ pipe_networking.c
@@ -0,0 +1,77 @@
+#include "pipe_networking.h"
+
+#define PUBLIC "fifo.pub" //visible to all
+#define PRIVATE "fifo"    //visible only to client
+
+/*=========================
+  server_handshake
+  args: int * to_client
+
+  Perofrms the client side pipe 3 way handshake.
+  Sets *to_client to the file descriptor to the downstream pipe.
+
+  returns the file descriptor for the upstream pipe.
+  =========================*/
+int server_handshake(int *to_client_pt) {
+  
+  mkfifo(PUBLIC, 0666);
+
+  if (fork() == 0) {
+    execlp("ls", "ls", NULL);
+  }
+  
+  char input[32];
+  int from_client = open(PUBLIC, O_RDONLY);
+  read(from_client, input, sizeof(input));
+  
+  remove(PUBLIC);
+
+  char output[32];
+  sprintf(output, "server recieved: '%s'", input);
+  printf("STEP ONE: %s\n", output);
+  int to_client = open(input, O_WRONLY); //input should be $PRIVATE
+  write(to_client, output, sizeof(output));
+  printf("STEP TWO: server sent: '%s'\n", output);
+
+  read(from_client, input, sizeof(input));
+  printf("STEP THREE: server recieved: '%s'\n", input);
+
+  *to_client_pt = to_client;
+  
+  return from_client;
+}
+
+
+/*=========================
+  client_handshake
+  args: int * to_server
+
+  Perofrms the client side pipe 3 way handshake.
+  Sets *to_server to the file descriptor for the upstream pipe.
+
+  returns the file descriptor for the downstream pipe.
+  =========================*/
+int client_handshake(int *to_server_pt) {
+
+  mkfifo(PRIVATE, 0666);
+  
+  int to_server = open(PUBLIC, O_WRONLY);
+  write(to_server, PRIVATE, sizeof(PRIVATE));
+  printf("STEP ONE: client sent: '%s'\n", PRIVATE);
+
+  char input[32];
+  int from_server = open(PRIVATE, O_RDONLY);
+  read(from_server, input, sizeof(input));
+
+  remove(PRIVATE);
+
+  char output[32];
+  sprintf(output, "client recieved: '%s'", input);
+  printf("STEP TWO: %s\n", input);
+  write(to_server, output, sizeof(output));
+  printf("STEP THREE: client sent: '%s'", output);
+
+  *to_server_pt = to_server;
+  
+  return from_server;
+}
